197d2ad56f564ce924a5e728b3fdd31fa0ed1926
yumishikawa <yum2712.ishikawa@gmail.com>
2024-01-24 23:53:41 +0900

    login

---
+++ app/models/public.rb
@@ -0,0 +1,10 @@
+class Public < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  belongs_to :user
+
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :validatable
+
+  
+end
